48ac9fbfdfd132ae9dfc0af10cc7ab357a05f839
Luiza Macena Dantas <luiza.macena2013@gmail.com>
2025-11-17 15:00:54 -0300

    add

--- skill-link/src/main/java/com/example/skill_link/controller/MatriculaController.java
+++ skill-link/src/main/java/com/example/skill_link/controller/MatriculaController.java
@@ -1,16 +1,23 @@
 package com.example.skill_link.controller;
 
-import com.example.skill_link.model.Competencia;
-import com.example.skill_link.model.Matricula;
-import com.example.skill_link.model.Usuario;
-import com.example.skill_link.service.MatriculaService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.skill_link.model.Matricula;
+import com.example.skill_link.service.MatriculaService;
+
+import jakarta.validation.Valid;
 
 @RestController @RequestMapping("/api/matriculas")
 public class MatriculaController {
